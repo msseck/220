bbce1bd2db267eb5b1d15ae8c6c18459019aba2f
msseck <seckms@g.cofc.edu>
2021-09-01 23:03:26 -0400

    completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: Marietou Seck
+hello.py
+
+Problem: <This program is meant to test my environment.>
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
