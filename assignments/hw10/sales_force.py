aaf6a3fa716505e137deaba824f078deb826fc04
msseck <seckms@g.cofc.edu>
2021-12-02 19:25:52 -0500

    completed hw 10

---
+++ assignments/hw10/sales_force.py
@@ -0,0 +1,67 @@
+"""
+Name:Marietou Seck
+sales_force.py
+
+Problem: This program demonstrates the use of objects with a list and list of objects.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work and discussed with: csl lab.
+"""
+from sales_person import *
+
+
+class SalesForce:
+    def __init__(self):
+        self.sales_people = []
+
+    def add_data(self, file_name):
+        in_file = open(file_name, 'r')
+        person_line = in_file.readlines()
+        for i in range(len(person_line)):
+            data = person_line[i].split(",")
+            person = SalesPerson(data[0], data[1])
+            sales = data[2].split()
+            for sale in sales:
+                person.enter_sale(sale)
+            self.sales_people.append(person)
+
+    def quota_report(self, quota):
+        report = []
+        for i in range(len(self.sales_people)):
+            report.append([self.sales_people[i].get_id(), self.sales_people[i].get_name(),
+                           self.sales_people[i].total_sales(),
+                           self.sales_people[i].total_sales() > quota])
+        return report
+
+    def top_seller(self):
+        seller = self.sales_people[0]
+        list_seller = []
+        for i in range(1, len(self.sales_people)):
+            if self.sales_people[i].compare_to(seller) == 1:
+                seller = self.sales_people[i]
+                list_seller = []
+        for i in range(len(self.sales_people)):
+            if self.sales_people[i].compare_to(seller) == 0:
+                list_seller.append(self.sales_people[i])
+        return list_seller
+
+    def individual_sales(self, employee_id):
+        for i in range(len(self.sales_people)):
+            if self.sales_people[i].get_id() == employee_id:
+                return self.sales_people[i]
+        return None
+
+
+# def main():
+#     sales = SalesForce()
+#     sales.add_data("salesData.txt")
+#     quota_sales = sales.quota_report(900)
+#     print(quota_sales)
+#     print("top seller(s): ")
+#     seller_list = sales.top_seller()
+#     for i in seller_list:
+#         print(i)
+#     sales.individual_sales(123)
+#
+#
+# main()
---
+++ assignments/hw10/sales_person.py
@@ -0,0 +1,75 @@
+"""
+Name:Marietou Seck
+sales_person.py
+
+Problem: This program demonstrates the use of objects with a list and list of objects.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work and discussed with: csl lab.
+"""
+
+
+class SalesPerson:
+    def __init__(self, employee_id, name):
+        self.employee_id = employee_id
+        self.name = name
+        self.sales = []
+
+    def get_id(self):
+        return self.employee_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def enter_sale(self, sale):
+        self.sales.append(float(sale))
+
+    def total_sales(self):
+        total = 0
+        for i in self.sales:
+            total = total + i
+        return total
+
+    def get_sales(self):
+        return self.sales
+
+    def met_quota(self, quota):
+        if self.total_sales() >= quota:
+            return True
+        return False
+
+    def compare_to(self, other):
+        if other.total_sales() > self.total_sales():
+            return -1
+        elif self.total_sales() > other.total_sales():
+            return 1
+        else:
+            return 0
+
+    def __str__(self):
+        return "{0}-{1}: {2}".format(self.employee_id, self.name, self.total_sales())
+
+
+# def main():
+#     sales = SalesPerson(10, "bob")
+#     print(sales.get_id())
+#     print(sales.get_name())
+#     sales.set_name("billy")
+#     print(sales.get_name())
+#     sales.enter_sale(2.75)
+#     sales.enter_sale(309.21)
+#     sales.enter_sale(42.37)
+#     print(sales.total_sales())
+#     print(sales.get_sales())
+#     print(sales.met_quota(396.7))
+#     sales1 = SalesPerson(14, "holly")
+#     sales1.enter_sale(2.03)
+#     sales1.enter_sale(305.21)
+#     sales1.enter_sale(57.37)
+#     print(sales.compare_to(sales1))
+#     print(sales)
+#
+# main()
